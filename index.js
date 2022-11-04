5795f566f3c8c2daab64daebe9f713d04fd7412d
mvs22 <mvs2@cin.ufpe.br>
2022-11-04 19:29:18 -0300

    Entrega final

--- index.js
+++ index.js
@@ -29,23 +29,67 @@ app.get('/', (req,res) => {
     res.render('index.ejs');
 })
 
-//app.get('/',(req,res) => {
-//    var cursor = db.collection('multimidia').find()
-//});
+app.get('/',(req,res) => {
+    var cursor = db.collection('multimidia').find()
+});
 
-//app.get('/show', (req,res) => {
-//    db.collection('multimidia').find().toArray((err,results) => {
-//        if (err) return console.log(err)
-//        //console.log(results)
-//        res.render('show.ejs', {data: results})
-//   });
-//});
+app.get('/show', (req,res) => {
+    //ADICIONAR VALIDADOR PARA IDENTIFICAR SE O RESULTS É VALIDO
+    db.collection('multimidia').find().toArray((err,results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', {data: results})
+   });
+});
 
-//app.post('/show', (req,res) => {
-//    db.collection('multimidia').insertOne({"cod":req.body.cod,"lat":req.body.lat,"long":req.body.long}, (err, result) => {
-//        if (err) return console.log(err)
+app.post('/show', (req,res) => {
+    //ADICIONAR VALIDADOR PARA IDENTIFICAR SE O RESULTS É VALIDO
+    db.collection('multimidia').find({"cod":req.body.cod}).toArray((err,results) => {
+        if (err) return console.log(err)
+        if (results == []) res.redirect('/')
+            console.log('Error cod')
+        res.render('show.ejs', {data: results})
+   });
+});
 
-//        console.log('Salvamento no banco concluído')
-//        res.redirect('/show')
-//    });
-//});
+app.post('/', (req,res) => {
+    db.collection('multimidia').insertOne({"cod":req.body.cod,"points":req.body.points}, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('Salvamento no banco concluído')
+        res.redirect('/')
+    });
+});
+
+
+app.route('/edit/:id').get((req,res) => {
+    var id = req.params.id
+    var ObjectId = require('mongodb').ObjectId;
+
+    console.log(id)
+    console.log(ObjectId(id))
+    db.collection('multimidia').find(ObjectId(id)).toArray((err,result) => {
+        if (err) return res.send(err)
+        res.render('edit.ejs', {data: result})
+    })
+}).post((req,res) => {
+    var id = req.params.id
+    var cod = req.body.cod
+    var points = req.body.points
+    var ObjectId = require('mongodb').ObjectId;
+
+    db.collection('multimidia').updateOne({_id: ObjectId(id)}, {$set: {"cod": cod, "points":points}},(err,result) => {
+        if (err) return res.send(err)
+        res.redirect('/')
+        console.log('Atualizado no Banco de Dados')
+    })
+})
+
+app.route('/delete/:id').get((req,res) => {
+    var id = req.params.id
+
+    db.collection('multimidia').deleteOne({'_id': ObjectId(id)}, (err,result) => {
+        if (err) return res.send(500,err)
+        console.log('Deletado do Banco de Dados!')
+        res.redirect('/')
+    })
+})
